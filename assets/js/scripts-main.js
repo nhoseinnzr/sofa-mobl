882d190dc97b704854316b4d7dbe55cdf668593d
Hosein <hoseinnazari137300@gmail.com>
2025-04-27 11:26:31 +0330

    First

---
+++ assets/js/scripts-main.js
@@ -0,0 +1,73 @@
+$('.owl-carousel2').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        767: {
+            items: 3,
+        },
+        1000: {
+            items: 6,
+        }
+    }
+})
+$('.owl-blog').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        767: {
+            items: 3,
+        },
+        1000: {
+            items: 4,
+        }
+    }
+})
+$('.owl-main-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    items: 1,
+
+})
+$('.owl-products').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    items: 4,
+    rtl:true,
+    responsiveClass: true,
+    responsive: {
+        0: {
+            items: 1,
+            center:true,
+        },
+        767: {
+            items: 3,
+        },
+        1000: {
+            items: 4,
+            center:true
+        }
+    }
+
+})
+$('body').on('click','.div-more .btn',function (){
+    $(this).parents('.tags').addClass('expand')
+
+
+})
+
